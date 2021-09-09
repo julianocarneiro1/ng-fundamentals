b073cf8e1d9848dd4decabc464c4d2bd57f20677
Juliano <julianoomc@gmail.com>
2021-09-09 19:29:36 -0300

    Using Template Variables to interact with child components

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -13,16 +13,16 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
                 <span>&nbsp;</span>
                 <span>{{event.location.city}}, {{event.location.country}}</span>
             </div>
-            <button class="btn btn-primary" (click)="handleClickMe()">Click me!</button>
         </div>
     `
 })
 export class EventThumbnailComponent {
 
     @Input() event: any
-    @Output() eventClick = new EventEmitter()
+    someProperty: string = "some value"
 
-    handleClickMe() {
-        this.eventClick.emit(this.event.name)
+    logFoo() {
+        console.log('foo')
     }
+
 }
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,7 +6,9 @@ import { Component } from "@angular/core";
         <div>
             <h1>Upcoming Angular Events</h1>
             <hr />
-            <event-thumbnail (eventClick)="handleEventClicked($event)" [event]="event1"></event-thumbnail>
+            <event-thumbnail #thumbnail [event]="event1"></event-thumbnail>
+            <h3>{{thumbnail.someProperty}}</h3>
+            <button class="btn btn-primary" (click)="thumbnail.logFoo()">Log me some foo</button>
         </div>
     `
 })
@@ -24,10 +26,6 @@ export class EventsListComponent {
             city: 'London',
             country: 'England'
         }
-    }
-
-    handleEventClicked(data: any) {
-        console.log('received:', data)
     }
 
 }
