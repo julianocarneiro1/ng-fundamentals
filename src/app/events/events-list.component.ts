c2f21f41997bdbdcaf89e149ad55225744c05962
Juliano <julianoomc@gmail.com>
2021-09-09 19:46:10 -0300

    Styling Components

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,9 +6,7 @@ import { Component } from "@angular/core";
         <div>
             <h1>Upcoming Angular Events</h1>
             <hr />
-            <event-thumbnail #thumbnail [event]="event1"></event-thumbnail>
-            <h3>{{thumbnail.someProperty}}</h3>
-            <button class="btn btn-primary" (click)="thumbnail.logFoo()">Log me some foo</button>
+            <event-thumbnail [event]="event1"></event-thumbnail>
         </div>
     `
 })
