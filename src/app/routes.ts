d71873376100bab304623b5d40b68c68ff6d3781
Juliano <julianoomc@gmail.com>
2021-09-10 15:00:00 -0300

    Guarding against route deactivation

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -7,7 +7,7 @@ import { EventsListComponent } from "./events/events-list.component";
 
 export const appRoutes: Routes = [
     { path: 'events', component: EventsListComponent },
-    { path: 'events/new', component: CreateEventComponent },
+    { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
     { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },    
     { path: '404', component: Error404Component },    
     { path: '', redirectTo: '/events', pathMatch: 'full' }
