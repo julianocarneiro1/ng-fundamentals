d62aab6e96dc98a387b05c3c50d4b540a12b6fe2
Juliano <julianoomc@gmail.com>
2021-09-10 11:31:35 -0300

    Creating service with mock data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent } from './events-app.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
+import { EventService } from './events/shared/event.service';
 import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
@@ -15,7 +16,8 @@ import { NavBarComponent } from './nav/navbar.component';
     EventsListComponent,
     EventThumbnailComponent,
     NavBarComponent
-  ],  
+  ],
+  providers: [EventService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
