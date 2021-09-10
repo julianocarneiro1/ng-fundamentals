045f67cbff1ce263ced8c2f28d92424d57fb2930
Juliano <julianoomc@gmail.com>
2021-09-10 13:32:41 -0300

    Accessing route parameters

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,21 +1,25 @@
 import { Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 import { EventService } from "../shared/event.service";
 
 @Component({
     templateUrl: './event-details.component.html',
     styles: [`
         .container { padding-left: 20px; padding-right: 20px; }
+        .event-image { height: 100px; }
     `]
 })
 export class EventDetailsComponent {
 
     event: any
 
-    constructor(private eventService: EventService) {
+    constructor(
+        private eventService: EventService,
+        private route: ActivatedRoute) {
     }
 
     ngOnInit() {
-        this.event = this.eventService.getEvent(1)
+        this.event = this.eventService.getEvent(+this.route.snapshot.params['id'])
     }
 
 }
