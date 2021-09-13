c54ba8fdca4316312cc25941912c618092f0f1ce
Juliano <julianoomc@gmail.com>
2021-09-13 12:08:18 -0300

    Using the data from template-based form to login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import {
 import { EventsAppComponent } from './events-app.component';
 import { NavBarComponent } from './nav/navbar.component';
 import { appRoutes } from './routes';
+import { AuthService } from './user/auth.service';
 
 @NgModule({
   imports: [
@@ -37,6 +38,7 @@ import { appRoutes } from './routes';
     ToastrService,
     EventRouteActivator,
     EventListResolver,
+    AuthService,
     {
       provide: 'canDeactivateCreateEvent',
       useValue: checkDirtyState
--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -1,16 +1,27 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "./auth.service";
 
 @Component({
     templateUrl: './login.component.html'
 })
 export class LoginComponent {
 
-
     userName: any;
     password: any;
 
+    constructor(
+        private authService: AuthService,
+        private router: Router) {
+    }
+
     login(formValues: any) {
-        console.log(formValues)
+        this.authService.loginUser(formValues.userName, formValues.password)
+        this.router.navigate(['events'])
+    }
+
+    cancel() {
+        this.router.navigate(['events'])
     }
 
 }
