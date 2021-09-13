0934d8eb30b818b7c816bef911b209b5d542be25
Juliano <julianoomc@gmail.com>
2021-09-13 15:38:13 -0300

    Building a template-based form to create new events

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,10 +18,13 @@ import { EventsAppComponent } from './events-app.component';
 import { NavBarComponent } from './nav/navbar.component';
 import { appRoutes } from './routes';
 import { AuthService } from './user/auth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   declarations: [
