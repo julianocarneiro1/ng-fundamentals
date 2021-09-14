62d1a614b7dad78a5a1f653b170753eae53955ff
Juliano <julianoomc@gmail.com>
2021-09-14 14:18:30 -0300

    Filtering data

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { EventService } from "../shared/event.service";
 import { IEvent, ISession } from "../shared/index";
@@ -12,10 +12,11 @@ import { IEvent, ISession } from "../shared/index";
         a { cursor: pointer }
     `]
 })
-export class EventDetailsComponent {
+export class EventDetailsComponent implements OnInit{
 
     event!: IEvent
     addMode!: boolean
+    filterBy: string = 'all'
 
     constructor(
         private eventService: EventService,
