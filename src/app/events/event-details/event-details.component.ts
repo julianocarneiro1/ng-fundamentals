835c9038e903f68369768c020582d2dd2e7ffacf
Juliano <julianoomc@gmail.com>
2021-09-14 16:12:28 -0300

    Sorting data

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -17,6 +17,7 @@ export class EventDetailsComponent implements OnInit{
     event!: IEvent
     addMode!: boolean
     filterBy: string = 'all'
+    sortBy: string = 'votes'
 
     constructor(
         private eventService: EventService,
--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -9,11 +9,13 @@ export class SessionListComponent implements OnChanges {
 
     @Input() sessions!: ISession[] | undefined
     @Input() filterBy!: string;
+    @Input() sortBy!: string;
     visibleSessions: ISession[] | undefined = []
 
     ngOnChanges() {
         if(this.sessions) {
             this.filterSessions(this.filterBy)
+            this.sortBy === 'name' ? this.visibleSessions?.sort(sortByNameAsc) : this.visibleSessions?.sort(sortByVotesDesc)
         }
     }
 
@@ -26,4 +28,14 @@ export class SessionListComponent implements OnChanges {
             })
         }
     }
+}
+
+function sortByNameAsc(s1: ISession, s2: ISession) {
+    if(s1.name > s2.name) return 1
+    else if(s1.name === s2.name) return 0
+    else return -1
+}
+
+function sortByVotesDesc(s1: ISession, s2: ISession) {
+    return s2.voters.length - s1.voters.length
 }
