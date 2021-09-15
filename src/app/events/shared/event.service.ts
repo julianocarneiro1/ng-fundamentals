9fa046b05fa28e9f7bf37f5a1f689358e5fbc326
Juliano <julianoomc@gmail.com>
2021-09-15 11:50:04 -0300

    Implementing the Session Search

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from "@angular/core";
+import { Injectable, EventEmitter } from "@angular/core";
 import { Observable, Subject } from "rxjs";
-import { IEvent } from ".";
+import { IEvent, ISession } from ".";
 
 @Injectable()
 export class EventService {
@@ -23,6 +23,27 @@ export class EventService {
     updateEvent(event: any) {
         let index = EVENTS.findIndex(x => x.id = event.id)
         EVENTS[index] = event
+    }
+
+    searchSessions(searchTerm: string) {
+        var term = searchTerm.toLocaleLowerCase()
+        var results: ISession[] = []
+
+        EVENTS.forEach(event => {
+            var matchingSessions = event.sessions.filter(session =>
+                session.name.toLocaleLowerCase().indexOf(term) > -1)
+            matchingSessions = matchingSessions.map((session: any) => {
+                session.eventId = event.id
+                return session
+            })
+            results = results.concat(matchingSessions)
+        })
+
+        var emitter = new EventEmitter(true)
+        setTimeout(() => {
+            emitter.emit(results)
+        }, 100)
+        return emitter
     }
 }
 
--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { EventService, ISession } from "../events";
 import { AuthService } from "../user/auth.service";
 
 @Component({
@@ -13,7 +14,20 @@ import { AuthService } from "../user/auth.service";
 })
 export class NavBarComponent {
 
-    constructor(public auth: AuthService) {
+    searchTerm: string = ""
+    foundSessions!: ISession[]
+
+    constructor(
+        public auth: AuthService,
+        private eventService: EventService) {
+    }
+
+    searchSessions(searchTerm: string) {
+        this.eventService.searchSessions(searchTerm)
+            .subscribe((sessions: ISession[]) => {
+                this.foundSessions = sessions
+                console.log(this.foundSessions)
+            })
     }
 
 }
