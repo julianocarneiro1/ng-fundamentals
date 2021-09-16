b6382973a6763131f5e7c1f233f779062ff5a7a6
Juliano <julianoomc@gmail.com>
2021-09-16 08:50:45 -0300

    Using @ViewChild to access and hide the modal with search results when a session is clicked

--- src/app/common/simple-modal.component.ts
+++ src/app/common/simple-modal.component.ts
@@ -1,16 +1,17 @@
-import { Component, Input } from "@angular/core";
+import { Component, ElementRef, Inject, Input, ViewChild } from "@angular/core";
+import { JQ_TOKEN } from './jQuery.service'
 
 @Component({
     selector: 'simple-modal',
     template: `
-        <div id="{{elementId}}" class="modal fade" tabindex="-1">
+        <div id="{{elementId}}" #modalcontainer class="modal fade" tabindex="-1">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
                         <h4 class="modal-title">{{title}}</h4>
                     </div>
-                    <div class="modal.body">
+                    <div class="modal-body" (click)="closeModal()">
                         <ng-content></ng-content>
                     </div>
                 </div>
@@ -25,4 +26,12 @@ export class SimpleModalComponent {
 
     @Input() title!: string
     @Input() elementId!: string
+    @ViewChild('modalcontainer') containerEl!: ElementRef
+
+    constructor(@Inject(JQ_TOKEN) private $: any) {
+    }
+
+    closeModal() {
+        this.$(this.containerEl.nativeElement).modal('hide')
+    }
 }
